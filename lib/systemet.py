af75ad5311e99ae39f2d0d7b1f57b36b12fa00ff
Stewart Rutledge <sterut@stebian.kb.local>
2013-08-30 09:56:11 +0200

    Removed numpy as averages are pretty easy to figure out without another dependency...

--- lib/systemet.py
+++ lib/systemet.py
@@ -2,7 +2,6 @@
 
 from pyelasticsearch import ElasticSearch
 from pymongo import MongoClient
-import numpy
 
 es = ElasticSearch('http://localhost:9200/')
 mongo_client = MongoClient()
@@ -44,4 +43,5 @@ def add_rating(articleid=None, rating=None):
                     {"$push": {"ratings": rating}})
     db.posts.update({"name": articleid},
                     {"$set": {"current_rating":
-                              round(numpy.mean(post.get("ratings")), 2)}})
+                              round(float(sum(post.get("ratings")))/len(post.get("ratings") 
+                                    if len(post.get("ratings")) > 0 else float(0)), 2)}})
